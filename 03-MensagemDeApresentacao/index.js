756189f9d071b039834ac0a1828388d5c1a6b9db
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-27 20:43:16 -0300

    Exercise 03 realized

---
+++ 03-MensagemDeApresentacao/index.js
@@ -0,0 +1,11 @@
+const usuario = {
+    nome: "Adriele",
+    idade: 23,
+    profissao: "programadora",
+    corPreferida: "ocre"
+}
+
+const {nome, idade, profissao, corPreferida} = usuario;
+console.log(`Olá...Eu 
+sou ${nome} e tenho ${idade} anos de idade. Sou ${profissao} e
+minha cor preferida é ${corPreferida}`)
