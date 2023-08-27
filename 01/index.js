40d2ae4f07b7a164ce330141d0ebeee486305ce3
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-27 20:16:03 -0300

    Project created

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "San verdade",
+    numero: 235,
+    complemento: "terreo",
+    bairro: "Chique-Chique",
+    cep: 42342456,
+    cidade: "Sarapatel",
+    estado: "Bahia",
+    pais: "Brasil"
+};
+
+const usuario = {
+    nome: "Sanji Oliveira",
+    email: "sanji.delas@gmail.com",
+    telefone: 75994577452,
+    data_nascimento: "08/03/2000", 
+    endereco
+};
+
+console.log(`Seja bem-vindo(a) ${usuario.nome}`)
