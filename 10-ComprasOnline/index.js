c381f532e9167f1e1232ee259e8d33ceba9f73cc
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-28 17:09:24 -0300

    Exercise about manipulating objects

---
+++ 10-ComprasOnline/index.js
@@ -0,0 +1,103 @@
+const produtos = [{
+    
+     nome: "TV Samsung 4K", 
+     valorEmCentavos: 129900, }
+, {
+  
+     nome: "Notebook Dell", 
+     valorEmCentavos: 399990, }
+, {
+
+    nome: "Mouse MX Master 3", 
+    valorEmCentavos: 23000, }
+, {
+   
+    nome: "Teclado Keychron K8", 
+    valorEmCentavos: 50000, }
+, {
+  
+    nome: "Cabo USB 2 Metros", 
+    valorEmCentavos: 1990, }
+, {
+   
+    nome: "Carregador portátil", 
+    valorEmCentavos: 4590, }
+, {
+   
+    nome: "Webcam C920s", 
+    valorEmCentavos: 80000, }
+];
+//-----------------------------------------------------
+const clientes = [{ 
+        nome: "Patricia",
+        carrinho: [{
+            nome: "Teclado Keychron K8",
+            valorEmCentavos: 50000,
+            quantidade: 1,
+        }, {
+            nome: "Cabo USB 2 Metros",
+            valorEmCentavos:1990,
+            quantidade: 2,
+        }, {
+            nome: "Carregador Portátil",
+            valorEmCentavos: 4590,
+            quantidade: 1
+        }, {
+            nome: "Mouse MX Master 3",
+            valorEmCentavos: 50000,
+            quantidade: 1,
+        }, {
+            nome: "Monitor LG 29 FHD",
+            valorEmCentavos: 129900,
+            quantidade: 1,
+        }],
+    },{
+   
+        nome: "Carlos",
+        carrinho: [{
+            nome: "Notebook Dell",
+            valorEmCentavos: 399990,
+            quantidade: 2,
+        },],
+    }, {
+        nome: "Renato", 
+        carrinho: [{
+            nome: "Webcam C920s",
+            valorEmCentavos: 80000,
+            quantidade: 5,
+        }],
+    }, {
+         nome: "José",
+        carrinho: [{
+            nome: "Tv Samsung 4k",
+            valorEmCentavos: 129900,
+            quantidade: 1,
+            
+        }, {
+            nome: "Cabo USB 2 Metros",
+            valorEmCentavos:1990,
+            quantidade: 2,
+
+        }, {
+            nome: "Webcam C920s",
+            valorEmCentavos:80000,
+            quantidade: 1,
+        }],
+    }, {
+   
+        nome: "Roberto",
+        carrinho: [{
+            nome: "Webcam C920s",
+            valorEmCentavos: 80000,
+            quantidade: 1,
+        }, {
+            nome: "Cabo USB 2 Metros",
+            valorEmCentavos: 1990,
+            quantidade: 2,
+        }, {
+            nome: "Monitor LG 29 FHD",
+            valorEmCentavos: 129900,
+            quantidade: 1,
+        }],  
+},];
+
